12d1099d9239a635260b4cb9f1793168fdd29183
유수현 <youg4231998@naver.com>
2024-10-14 19:50:39 +0900

    fix player input add state

--- Mario/AiAgent.py
+++ Mario/AiAgent.py
@@ -1,5 +1,7 @@
 import torch
 import random
+import pygame as pg
+from pygame.locals import *
 import numpy as np
 from collections import deque
 from MarioEnv import MarioEnv
@@ -19,14 +21,13 @@ class Agent:
         self.epsilon = 0 #randomness
         self.gamma = 0.9 #discount rate #must be smaller than 1
         self.memory = deque(maxlen=MAX_MEMORY) #popleft
-        self.model = Linear_QNet(11, 256, 3)
+        self.model = Linear_QNet(9, 256, 5)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
-        #TODO: model, trainer
 
     def get_state(self, game):
         # players pos
         player_x = game.core.get_map().get_player().pos_x  # 플레이어의 X 좌표
-        player_y = game.core.get_map().get_player().pos_y  # 플레이어의 Y 좌표
+        player_y = game.core.get_map().get_player().rect.y  # 플레이어의 Y 좌표
 
         # goal x pos
         goal_x = 6341.25
@@ -34,45 +35,48 @@ class Agent:
         # distance of goal and player
         distance_to_goal = goal_x - player_x
 
-        # enemies and obstacles pos
-        enemies = game.core.get_map().get_mobs_xPos()  # 적들의 좌표 리스트 [(x1, y1), (x2, y2), ...]
-        #obstacles = game.core.get_obstacles()  # 장애물 좌표 리스트 [(x1, y1), (x2, y2), ...]
+        #hazard y cord
+        hazard_y = 448
+
+
+        #enemies pos
+        enemies = game.core.get_map().get_mobs_xPos()
 
         # distance between enemies
-        closest_enemy_distance = float('inf')
-
         #only if the enemies exists
         if enemies != None:
+            closest_x = enemies[0]
             for enemy in enemies:
-                enemy_x = enemy
-                distance = enemy_x - player_x
-                if distance > 0:  # 오른쪽에 있는 적만 계산
-                    closest_enemy_distance = min(closest_enemy_distance, distance)
+                distance = enemy
+                if distance < closest_x:
+                    closest_x = distance
+        else:
+            closest_x = -1
 
-        #TODO:
+        #blocks pos
+        ob_blocks = game.core.get_map().get_near_blocks()
+        cha_right = ob_blocks[0]
+        cha_left = ob_blocks[1]
+        cha_upper = ob_blocks[2]
+        cha_upper_r = ob_blocks[3]
+        # down = ob_blocks[3]
 
-        # distance between obstacles
-        # closest_obstacle_distance = float('inf')
-        # for obstacle in obstacles:
-        #     obstacle_x, obstacle_y = obstacle
-        #     distance = obstacle_x - player_x
-        #     if distance > 0:  # 오른쪽에 있는 장애물만 계산
-        #         closest_obstacle_distance = min(closest_obstacle_distance, distance)
-
-        #TODO
-        # hazard zones
-        #is_near_cliff = self.core.is_near_cliff(player_x, player_y)  # 플레이어가 낭떠러지 근처에 있는지 확인
-
-        # 상태 벡터로 만들어 반환
+        # state vector
         state = np.array([
-            player_x,  # 플레이어 X 좌표
-            player_y,  # 플레이어 Y 좌표
-            distance_to_goal,  # 골대까지의 거리
-            closest_enemy_distance,  # 가장 가까운 적과의 거리
-            #closest_obstacle_distance,  # 가장 가까운 장애물과의 거리
-            #int(is_near_cliff)  # 낭떠러지 근처인지 여부 (1이면 낭떠러지 근처, 0이면 아님)
+            player_x,  # player x pos
+            player_y,  # player y pos
+            distance_to_goal,  # left distance x to goal
+            hazard_y,           #out from map y cord
+            player_x < closest_x,  # 가장 가까운 적과의 거리
+            #player surronding blocks
+            cha_right,
+            cha_left,
+            cha_upper,
+            cha_upper_r,
         ])
-
+        # print('x: ',state[0],' y: ',state[1],' enemy: ',state[4],
+        #        'r: ', state[5],' l: ',state[6],
+        #       ' u: ', state[7], ' ur: ', state[8])
         return state
 
     def remember(self, state, action, reward, next_state, done):
@@ -91,37 +95,28 @@ class Agent:
 
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        #TODO
-        self.trainer.train_step( state, action, reward, next_state, done)
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        #random moves
-        self.epsilon = 80 - self.num_games
-        #init value
-        final_move = 0
+        # random moves
+        #self.epsilon = 80 - self.num_games
+        self.epsilon = max(0.01, 80 - self.num_games)
+        final_move = [0, 1, 2, 3, 4]
+        action_num = 0
+
+        # init value
         if random.randint(0, 200) < self.epsilon:
-            final_move = random.randint(0, 4)
+            move = random.randint(1, 4)
+            action_num = final_move[move]
         else:
-            #tensor
+            # tensor
             first_state = torch.tensor(state, dtype=torch.float)
             prediction = self.model(first_state)
-            final_move = torch.argmax(prediction).item()
+            move = torch.argmax(prediction).item()
+            action_num = final_move[move]
 
-        return final_move
-    # def get_action(self, state):
-    #     # random moves: tradeoff exploration / exploitation
-    #     self.epsilon = 80 - self.num_games
-    #     final_move = [0, 0, 0, 0, 0]  # 5 actions
-    #     if random.randint(0, 200) < self.epsilon:
-    #         move = random.randint(0, 4)  # Now chooses from 0 to 4
-    #         final_move[move] = 1
-    #     else:
-    #         state0 = torch.tensor(state, dtype=torch.float)
-    #         prediction = self.model(state0)
-    #         move = torch.argmax(prediction).item()
-    #         final_move[move] = 1
-    #
-    #     return final_move
+        #print(action_num)
+        return action_num
 
 def train():
     plot_scores = []
@@ -130,6 +125,7 @@ def train():
     record = 0
     agent = Agent()
     game = MarioEnv()
+
 
     while True:
         #get old state
@@ -147,7 +143,10 @@ def train():
 
         #remember
         agent.remember(state_old, final_move, reward, state_new, done)
+
+        #render
         game.render()
+
         if done:
             # train long memory, plot result
             game.reset()
@@ -156,8 +155,8 @@ def train():
 
             if score > record:
                 record = score
-                #agent.model.save()
-            print('Game', agent.num_games, 'Score', score, 'Record: ', record)
+                agent.model.save()
+            print('Game', agent.num_games, 'Score', score, 'Record: ', record, 'Reward: ', reward)
 
             plot_scores.append(score)
             total_score += score
--- Mario/MarioEnv.py
+++ Mario/MarioEnv.py
@@ -6,6 +6,8 @@ import cv2
 import random
 import time
 import pygame as pg
+
+import Const
 from Const import *
 from gym.core import RenderFrame
 from enum import Enum
@@ -19,17 +21,8 @@ from pygame.locals import *
 from Core import Core
 
 
-#Mario_LEN_GOAL = 30
-
-class MOVES(Enum):
-    RIGHT = 0
-    LEFT = 1
-    JUMP = 2
-    JUMP_LEFT = 3
-    JUMP_RIGHT = 4
-
 class MarioEnv(gym.Env, object):
-    reward = 0
+    render_on = True
     def __init__(self):
         #print("init")
         super(MarioEnv, self).__init__()
@@ -38,25 +31,24 @@ class MarioEnv(gym.Env, object):
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions:
-        self.action_space = spaces.Discrete(5)
+        #self.action_space = spaces.Discrete(5)
         # Example for using image as input (channel-first; channel-last also works):
-        self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(WINDOW_H, WINDOW_W, 3),
-                                            dtype=np.uint8)
-        self.currentTime = 0
-        self.input_cooldown = 200
-        self.score = 0
-        self.direction = MOVES.RIGHT
+        # self.observation_space = spaces.Box(low=0, high=255,
+        #                                     shape=(WINDOW_H, WINDOW_W, 3),
+        #                                     dtype=np.uint8)
 
-    def reset(self):
-        print("reset")
-        self.direction = MOVES.RIGHT
-        self.last_input_time = 0
-        self.currentTime = 0
-        self.input_cooldown = 200
         self.reward = 0
         self.score = 0
+        self.flag_x_position = 6341.25
+        self.init_distance = self.flag_x_position - self.core.get_map().get_player().pos_x
+        self.previous_distance_to_goal = self.init_distance
+
+    def reset(self):
+        self.reward = 0
+        self.score = 0
+        #init game
         self.core.restart_game()
+        self.core.stop()
 
     def step(self, action):
         #1. user input
@@ -66,51 +58,73 @@ class MarioEnv(gym.Env, object):
                 quit()
 
         # 2. move
-        #input delay
-        if self.currentTime <= 0:
-            self.handle_action(action)
-            self.currentTime = self.input_cooldown
-        #input delay cooldown
-        self.currentTime = self.currentTime - 1
+        #no delay
+        self.handle_action(action)
 
         #game update
         self.core.update()
-
+        self.input_player()
+        if (self.core.get_map().get_player().on_ground and
+                self.core.get_map().get_player().already_jumped):
+            self.core.keyU = False
 
         #3. check is game over
         done = False
+        self.reward = 0
 
-        reward = 0
-
-        #player dead or time up
-        if self.player_fails() or self.core.get_time() <= 0:
-            reward -= 10
+        #4. player dead
+        if self.player_fails():
             done = True
-            self.score = self.core.get_score()
-            return reward, done, self.score
-        #game clear
+            self.reward -= 50
+            self.score = self.get_player_dist_score()
+            return self.reward, done, self.score
+
+        #5. time up
+        if self.core.get_time() <= 0:
+            done = True
+            self.reward -= 30
+            self.score = self.get_player_dist_score()
+            return self.reward, done, self.score
+
+        #6. game clear
         if self.player_clear():
             done = True
-            reward += 10
-            self.score = self.core.get_score()
-            return reward, done, self.score
+            self.reward += 200
+            self.score = (100 + self.get_player_dist_score() +
+                          self.core.get_score())
+            return self.reward, done, self.score
+
+        # # 7. intermediate rewards
+        # distance_to_goal = self.get_distance_to_goal()
+        # time_remaining = self.core.get_time()
+        #
+        # previous_distance = self.previous_distance_to_goal
+        # self.previous_distance_to_goal = distance_to_goal
+        #
+        # if distance_to_goal < previous_distance:
+        #     self.reward += 0.1  # 목표에 가까워질 때마다 소량의 보상
+        #
+        # # 시간이 흐를수록 작은 페널티 부여 (빠른 목표 달성을 유도)
+        # self.reward -= 0.01 * (100 - time_remaining)
+        #
+        # print(self.reward)
 
         #4. return
-        return reward, done, self.score
+        return self.reward, done, self.score
 
-    # def calculate_reward(self):
-    #     reward = 0
-    #     if self.core.get_mm().currentGameState == 'Game':
-    #         reward += 1  # 생존 보상
-    #     return reward
+    def get_player_dist_score(self):
+        distance = self.get_distance_to_goal()
+        score = (self.init_distance - distance) / self.init_distance
+        return round(score, 2) * 10
 
-    # def set_reward(self, score):
-    #     self.score = score
+    def get_distance_to_goal(self):
+        player_x = self.core.get_map().get_player().pos_x
+        distance = self.flag_x_position - player_x
+        return round(distance, 2)
 
     def player_fails(self):
         if self.core.get_map().get_event().game_over:
             print("player dead")
-            #self.set_reward(-100)
             return True
         return False
 
@@ -120,29 +134,116 @@ class MarioEnv(gym.Env, object):
         return False
 
     def handle_action(self, action):
-        if action == 0:
-            self.core.move_right()
-        elif action == 1:
-            self.core.move_left()
-        elif action == 2:  # 점프
-            self.core.jump()
-        elif action == 3:
-            self.core.jump_left()
-        elif action == 4:
-            self.core.jump_right()
+        #[right, left, jump, jump_right, jump_left]
+        # directions = [Direction.RIGHT, Direction.LEFT,
+        #               Direction.JUMP, Direction.JUMP_RIGHT]
+        # idx = directions.index(self.direction)
+        # k = pg.key.get_pressed()
+        # #player moves
+        # if np.array_equal(action, [1, 0, 0]): #right
+        #     #self.core.move_right()
+        #     k[K_d] = True
+        #     #new_dir = directions[idx]
+        # elif np.array_equal(action, [0, 1, 0]): #left
+        #     k[K_a] = True
+        #     # next_idx = (idx + 1) % 4
+        #     # new_dir = directions[next_idx]
+        # else:                       #jump
+        #     k[K_w] = True
+        #     #self.core.jump()
+        #     # next_idx = (idx - 1) % 4
+        #     # new_dir = directions[next_idx]
+        # k[K_d] = False
+        # k[K_a] = False
+        # k[K_w] = False
 
+        # self.direction = new_dir
 
-    def get_state(self):
-        state = pg.surfarray.array3d(self.core.screen)
-        return np.transpose(state, (1, 0, 2))  # 상태 형식을 (높이, 너비, 채널)로 변환
+        # if self.direction == Direction.RIGHT:
+        #     self.core.move_right()
+        # elif self.direction == Direction.LEFT:
+        #     self.core.jump_right()
+        # elif self.direction == Direction.JUMP:
+        #     self.core.jump()
+        # elif self.direction == Direction.JUMP_RIGHT:
+        #     self.core.jump_right()
+        #action = 3
+        # 2. action 값에 따라 마리오의 행동을 제어
+        if action == 0:  # 아무 동작도 하지 않음
+            # 아무런 키 입력도 발생시키지 않음
+            #print('right')
+            self.press_key(pg.K_RIGHT)  # 오른쪽 방향키 누르기
+            self.release_key(pg.K_LEFT)
+            self.press_key(pg.K_UP)
+        elif action == 1:  # 왼쪽 이동
+            #print('left')
+            self.press_key(pg.K_LEFT)  # 왼쪽 방향키 누르기
+            self.release_key(pg.K_RIGHT)
+            self.release_key(pg.K_UP)
+        elif action == 2:  # 오른쪽 이동
+            #print('right')
+            self.press_key(pg.K_RIGHT)  # 오른쪽 방향키 누르기
+            self.release_key(pg.K_LEFT)
+            self.release_key(pg.K_UP)
+        elif action == 3:  # 점프
+            self.press_key(pg.K_UP)  # 스페이스바 (점프) 누르기
+        elif action == 4:  # 오른쪽 이동 + 점프
+            self.press_key(pg.K_RIGHT)  # 오른쪽 방향키 누르기
+            self.release_key(pg.K_LEFT)
+            self.press_key(pg.K_UP)  # 스페이스바 (점프) 누르기
+
+        # 행동을 멈추기 위해 키를 떼는 부분 (optional)
+        if action in [1, 2]:  # 왼쪽이나 오른쪽으로 이동한 경우
+            self.release_key(pg.K_LEFT)
+            self.release_key(pg.K_RIGHT)
+        # elif action in [3, 4]:  # 점프를 한 경우
+        #     self.release_key(pg.K_UP)
+
+    def press_key(self, key):
+        # 키를 누르는 이벤트 생성
+        event = pg.event.Event(pg.KEYDOWN, key=key)
+        pg.event.post(event)
+
+    def release_key(self, key):
+        # 키를 떼는 이벤트 생성
+        event = pg.event.Event(pg.KEYUP, key=key)
+        pg.event.post(event)
+
+    def input_player(self):
+        for e in pg.event.get():
+            #print(e)
+            if e.type == pg.KEYDOWN:
+                #print('down')
+                if e.key == pg.K_RIGHT:
+                    self.core.keyR = True
+                elif e.key == pg.K_LEFT:
+                    self.core.keyL = True
+                elif e.key == pg.K_DOWN:
+                    self.core.keyD = True
+                elif e.key == pg.K_UP:
+                    self.core.keyU = True
+                elif e.key == pg.K_LSHIFT:
+                    self.core.keyShift = True
+
+            elif e.type == pg.KEYUP:
+                if e.key == pg.K_RIGHT:
+                    self.core.keyR = False
+                elif e.key == pg.K_LEFT:
+                    self.core.keyL = False
+                elif e.key == pg.K_DOWN:
+                    self.core.keyD = False
+                elif e.key == pg.K_UP:
+                    self.core.keyU = False
+                elif e.key == pg.K_LSHIFT:
+                    self.core.keyShift = False
 
     def render(self, mode='human'):
-        # Pygame 화면을 렌더링
+        #if self.render_on:
         self.core.render()
-        pg.display.flip()  # 화면 업데이트
+        pg.display.flip()
 
     def has_reached_flag(self):
-        # 깃대에 도달했는지 확인하는 로직을 구현
+        # check player has reach flag
         return self.core.reached_flag()
 
 # MarioEnv 등록
